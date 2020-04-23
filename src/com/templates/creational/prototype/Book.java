157d83e14f01e5893432767baa9097af7397b64a
Karol Mendza <karol.mendza@codino.pl>
2020-04-23 23:17:42 +0200

    First part of prototype pattern

---
+++ src/com/templates/creational/prototype/Book.java
@@ -0,0 +1,13 @@
+package com.templates.creational.prototype;
+
+public class Book extends Item {
+    private int pageNumber;
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+}
